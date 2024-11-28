6daf03688360c24fc92b801b1d9bb70494731a1c
User <nastena.gafurova02@gmail.com>
2024-11-28 18:55:19 +0500

    ;

--- HW_4/4.4.py
+++ HW_4/4.4.py
@@ -3,7 +3,6 @@ import requests
 
 if __name__ == '__main__':
     data = pd.DataFrame(columns=['Температура', 'Влажность'])
-    #следующий юрл был доработан под персональный апи код, координаты взять из открытых источникоd
     url = ("https://api.openweathermap.org/data/2.5/weather?lat=55.7522&lon=37.6156&appid"
            "=ce9f26431abc8888a5b3571c1fa08049")
     request = requests.get(url).json()
