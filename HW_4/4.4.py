6cb90181f62893fab6d161369f904dbd44ed34d2
Настя <106319755+gafurcha@users.noreply.github.com>
2024-12-03 17:38:52 +0500

    4.4.py

--- HW_4/4.4.py
+++ HW_4/4.4.py
@@ -4,7 +4,7 @@ import requests
 if __name__ == '__main__':
     data = pd.DataFrame(columns=['Температура', 'Влажность'])
     url = ("https://api.openweathermap.org/data/2.5/weather?lat=55.7522&lon=37.6156&appid"
-           "=ce9f26431abc8888a5b3571c1fa08049")
+           "=b7a81f1f92b648111c02c4d885d0f582")
     request = requests.get(url).json()
     temp = (float(request["main"]["temp"]) - 273.15)
     temp = round(temp, 2)
