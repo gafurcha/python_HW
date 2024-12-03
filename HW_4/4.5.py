2965b8f56b6ce1b5ac957ec017c363acaf63fbf8
Настя <106319755+gafurcha@users.noreply.github.com>
2024-12-03 17:39:10 +0500

    4.5.py

--- HW_4/4.5.py
+++ HW_4/4.5.py
@@ -3,7 +3,7 @@ import requests
 
 if __name__ == '__main__':
     url = ("https://api.openweathermap.org/data/2.5/weather?lat=55.7522&lon=37.6156&appid"
-           "=ce9f26431abc8888a5b3571c1fa08049")
+           "=b7a81f1f92b648111c02c4d885d0f582")
     for i in range(4):
         request = requests.get(url)
         if request.status_code != 200:
